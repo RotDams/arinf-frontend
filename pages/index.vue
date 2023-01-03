d4c1548e32bba7d505f6b12d73dc65fd38941fec
Damien Champeyroux <damien.champeyroux@epita.fr>
2023-01-03 16:39:30 +0100

    feat (app): add conter vue as default vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,30 @@
 <template>
-  <Tutorial/>
+  <h1>Hi! You are the visitor number {{visitors}}!</h1>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'IndexPage'
+  name: 'IndexPage',
+
+  async asyncData({ app }) {
+
+    const url = process.env.BACKEND_API + "/counter";
+    const data = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const { count } = await data.json()
+    return { visitors: count }
+  },
+
+  data() {
+    return {
+      visitors: 0
+    }
+  }
 })
 </script>
